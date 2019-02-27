c197a31871c44fab2749462f15e10881a93ea2a3
Akira Matsuda <ronnie@dio.jp>
2019-02-27 23:02:29 +0900

    Test with test-unit-rails

--- test/still_life_test.rb
+++ test/still_life_test.rb
@@ -1,6 +1,6 @@
 require "test_helper"
 
-class StillLifeTest < Minitest::Test
+class StillLifeTest < Test::Unit::TestCase
   def test_that_it_has_a_version_number
     refute_nil ::StillLife::VERSION
   end
