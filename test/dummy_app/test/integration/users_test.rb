ddeba3da50d2d5086f895457e8fe412051dbf03d
Akira Matsuda <ronnie@dio.jp>
2019-02-28 23:40:33 +0900

    A simple integration test

--- test/dummy_app/test/integration/users_test.rb
+++ test/dummy_app/test/integration/users_test.rb
@@ -1,7 +1,12 @@
 require 'test_helper'
 
 class UsersTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  setup do
+    @user = users(:one)
+  end
+
+  test 'it' do
+    get "/users/#{@user.id}"
+    assert_response :success
+  end
 end
