d4544bb6faf13b3ceb611acd72ccad10a26104cf
Akira Matsuda <ronnie@dio.jp>
2019-03-01 21:05:47 +0900

    Clear tmp HTML files

--- test/still_life_test.rb
+++ test/still_life_test.rb
@@ -3,6 +3,8 @@ require "test_helper"
 class StillLifeTest < Test::Unit::TestCase
   def test_executing_tests_in_the_dummy_app
     Dir.chdir "#{__dir__}/dummy_app" do
+      FileUtils.rm_r 'tmp/html/'
+
       Bundler.with_clean_env do
         system 'bundle e rails test'
 
