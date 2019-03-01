62d6fc6b0849af78eda92d53a6f414e9b924c9be
Akira Matsuda <ronnie@dio.jp>
2019-03-01 16:21:54 +0900

    Monkey-patch on http request methods to preserve each rendered response.body HTML to a file

--- lib/still_life.rb
+++ lib/still_life.rb
@@ -1,6 +1,24 @@
-require "still_life/version"
+# frozen_string_literal: true
+
+require_relative 'still_life/version'
 
 module StillLife
-  class Error < StandardError; end
-  # Your code goes here...
+  def self.record_html(html, location)
+    if html.present?
+      pathname = Rails.root.join("tmp/html/#{location.tr(':', '-')}.html")
+      pathname.parent.mkpath
+      pathname.write html
+    end
+  end
+
+  module ResponseBodyRecorder
+    %W(get post put patch delete).each do |meth|
+      define_method meth do |*args|
+        super(*args).tap do
+          location = caller.detect {|c| c =~ /^#{Rails.root}/}.remove(Rails.root.to_s, /:in .*$/)
+          StillLife.record_html(response.body, location)
+        end
+      end
+    end
+  end
 end
