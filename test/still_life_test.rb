583557b1f00c7da2f2eb484b8317d501dc70a725
Akira Matsuda <ronnie@dio.jp>
2019-03-01 21:01:23 +0900

    Assert that these files are generated through controller / integration tests

--- test/still_life_test.rb
+++ test/still_life_test.rb
@@ -5,6 +5,16 @@ class StillLifeTest < Test::Unit::TestCase
     Dir.chdir "#{__dir__}/dummy_app" do
       Bundler.with_clean_env do
         system 'bundle e rails test'
+
+        assert_html_dumped 'test/controllers/users_controller_test.rb-9'
+        assert_html_dumped 'test/controllers/users_controller_test.rb-14'
+        assert_html_dumped 'test/controllers/users_controller_test.rb-20'
+        assert_html_dumped 'test/controllers/users_controller_test.rb-27'
+        assert_html_dumped 'test/controllers/users_controller_test.rb-32'
+        assert_html_dumped 'test/controllers/users_controller_test.rb-37'
+        assert_html_dumped 'test/controllers/users_controller_test.rb-43'
+        assert_html_dumped 'test/integration/users_test.rb-9'
+
         system 'bundle e rails test:system'
       end
     end
