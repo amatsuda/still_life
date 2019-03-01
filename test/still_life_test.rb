68db5a4bdf5ae5d998e5fd35ad5d8994d839269b
Akira Matsuda <ronnie@dio.jp>
2019-03-02 06:06:48 +0900

    Assertions for system tests

--- test/still_life_test.rb
+++ test/still_life_test.rb
@@ -18,6 +18,18 @@ class StillLifeTest < Test::Unit::TestCase
         assert_html_dumped 'test/integration/users_test.rb-9'
 
         system 'bundle e rails test:system'
+
+        assert_html_dumped 'test/system/users_test.rb-9'
+        assert_html_dumped 'test/system/users_test.rb-14'
+        assert_html_dumped 'test/system/users_test.rb-15'
+        assert_html_dumped 'test/system/users_test.rb-18'
+        assert_html_dumped 'test/system/users_test.rb-21'
+        assert_html_dumped 'test/system/users_test.rb-25'
+        assert_html_dumped 'test/system/users_test.rb-26'
+        assert_html_dumped 'test/system/users_test.rb-29'
+        assert_html_dumped 'test/system/users_test.rb-32'
+        assert_html_dumped 'test/system/users_test.rb-36'
+        assert_html_dumped 'test/system/users_test.rb-37'
       end
     end
   end
