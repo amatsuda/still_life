95d62cf86717f46b1106b0afc13ebdcaedd63ae2
Akira Matsuda <ronnie@dio.jp>
2019-03-01 20:52:51 +0900

    Assertion that asserts dumped HTML file existence

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -5,3 +5,9 @@ require 'rails'
 require "still_life"
 
 require 'test/unit'
+
+Test::Unit::TestCase.class_eval do
+  def assert_html_dumped(path)
+    assert File.exist?(f = "#{__dir__}/dummy_app/tmp/html/#{path}.html") && File.read(f).present?, "#{f} does not exist"
+  end
+end
