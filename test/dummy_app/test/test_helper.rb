b6854067d8be5905af30a5781c43ac91ec752c1d
Akira Matsuda <ronnie@dio.jp>
2019-03-02 07:11:37 +0900

    Support test-unit-rails

--- test/dummy_app/test/test_helper.rb
+++ test/dummy_app/test/test_helper.rb
@@ -1,6 +1,12 @@
 ENV['RAILS_ENV'] ||= 'test'
 require_relative '../config/environment'
-require 'rails/test_help'
+
+case ENV['TEST_FRAMEWORK']
+when 'test-unit'
+  require 'test/unit/rails/test_help'
+else
+  require 'rails/test_help'
+end
 
 class ActiveSupport::TestCase
   # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
