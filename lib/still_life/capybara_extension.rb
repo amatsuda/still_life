dee6ce6df3562bcbfd33a0c55ea7f19417c45fc4
Akira Matsuda <ronnie@dio.jp>
2019-03-02 23:53:31 +0900

    You give each Module a good name

--- lib/still_life/capybara_extension.rb
+++ lib/still_life/capybara_extension.rb
@@ -2,54 +2,57 @@
 
 module StillLife
   module CapybaraExtension
+    module ElementExtension
+      def click(*)
+        return super if Thread.current[:_still_life_inside_modal]
+
+        body_was = session.body
+        super.tap do
+          session.find('body')
+          if session.body.present? && (session.body != body_was)
+            StillLife.draw(session.body)
+          end
+        end
+      end
+    end
+
+    module SessionExtension
+      def visit(*)
+        super.tap do
+          if body.present?
+            StillLife.draw(body)
+          end
+        end
+      end
+
+      private def accept_modal(*)
+        Thread.current[:_still_life_inside_modal] = true
+        body_was = body
+        super.tap do
+          if body.present? && (body != body_was)
+            StillLife.draw(body)
+          end
+        end
+      ensure
+        Thread.current[:_still_life_inside_modal] = nil
+      end
+
+      private def dismiss_modal(*)
+        Thread.current[:_still_life_inside_modal] = true
+        body_was = body
+        super.tap do
+          if body.present? && (body != body_was)
+            StillLife.draw(body)
+          end
+        end
+      ensure
+        Thread.current[:_still_life_inside_modal] = nil
+      end
+    end
+
     def self.prepended(kls)
-      Capybara::Node::Element.prepend Module.new {
-        def click(*)
-          return super if Thread.current[:_still_life_inside_modal]
-
-          body_was = session.body
-          super.tap do
-            session.find('body')
-            if session.body.present? && (session.body != body_was)
-              StillLife.draw(session.body)
-            end
-          end
-        end
-      }
-
-      Capybara::Session.prepend Module.new {
-        def visit(*)
-          super.tap do
-            if body.present?
-              StillLife.draw(body)
-            end
-          end
-        end
-
-        private def accept_modal(*)
-          Thread.current[:_still_life_inside_modal] = true
-          body_was = body
-          super.tap do
-            if body.present? && (body != body_was)
-              StillLife.draw(body)
-            end
-          end
-        ensure
-          Thread.current[:_still_life_inside_modal] = nil
-        end
-
-        private def dismiss_modal(*)
-          Thread.current[:_still_life_inside_modal] = true
-          body_was = body
-          super.tap do
-            if body.present? && (body != body_was)
-              StillLife.draw(body)
-            end
-          end
-        ensure
-          Thread.current[:_still_life_inside_modal] = nil
-        end
-      }
+      Capybara::Node::Element.prepend ElementExtension
+      Capybara::Session.prepend SessionExtension
     end
   end
 end
