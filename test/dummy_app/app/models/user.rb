2621f681c17c2e8af688b616de472410da11d773
Akira Matsuda <ronnie@dio.jp>
2019-03-02 17:40:18 +0900

    User.validates_presence_of :name
    for the controller specs to pass

--- test/dummy_app/app/models/user.rb
+++ test/dummy_app/app/models/user.rb
@@ -1,2 +1,3 @@
 class User < ApplicationRecord
+  validates_presence_of :name
 end
--- test/dummy_app/spec/controllers/users_controller_spec.rb
+++ test/dummy_app/spec/controllers/users_controller_spec.rb
@@ -29,11 +29,11 @@ RSpec.describe UsersController, type: :controller do
   # User. As you add validations to User, be sure to
   # adjust the attributes here as well.
   let(:valid_attributes) {
-    skip("Add a hash of attributes valid for your model")
+    {name: 'somebody'}
   }
 
   let(:invalid_attributes) {
-    skip("Add a hash of attributes invalid for your model")
+    {name: ''}
   }
 
   # This should return the minimal set of values that should be in the session
@@ -97,14 +97,14 @@ RSpec.describe UsersController, type: :controller do
   describe "PUT #update" do
     context "with valid params" do
       let(:new_attributes) {
-        skip("Add a hash of attributes valid for your model")
+        {name: 'somebody else'}
       }
 
       it "updates the requested user" do
         user = User.create! valid_attributes
         put :update, params: {id: user.to_param, user: new_attributes}, session: valid_session
         user.reload
-        skip("Add assertions for updated state")
+        expect(user.name).to eq('somebody else')
       end
 
       it "redirects to the user" do
@@ -137,5 +137,4 @@ RSpec.describe UsersController, type: :controller do
       expect(response).to redirect_to(users_url)
     end
   end
-
 end
