c5b57761ae928bf1d0a45a1091cedc7c7a7d21b1
Akira Matsuda <ronnie@dio.jp>
2019-03-02 20:29:14 +0900

    Execution and assertions for system specs

--- test/still_life_test.rb
+++ test/still_life_test.rb
@@ -35,23 +35,41 @@ class StillLifeTest < Test::Unit::TestCase
           assert_html_dumped 'test/integration/users_integration_test.rb-9'
         end
 
-        if ENV['TEST_FRAMEWORK'] == 'test-unit'
+        case ENV['TEST_FRAMEWORK']
+        when 'test-unit'
           system 'BUNDLE_GEMFILE=Gemfile.test-unit bundle e rails test:system'
+        when 'rspec'
+          system 'BUNDLE_GEMFILE=Gemfile.rspec bundle e rails spec:system'
         else
           system 'bundle e rails test:system'
         end
 
-        assert_html_dumped 'test/system/users_test.rb-9'
-        assert_html_dumped 'test/system/users_test.rb-14'
-        assert_html_dumped 'test/system/users_test.rb-15'
-        assert_html_dumped 'test/system/users_test.rb-18'
-        assert_html_dumped 'test/system/users_test.rb-21'
-        assert_html_dumped 'test/system/users_test.rb-25'
-        assert_html_dumped 'test/system/users_test.rb-26'
-        assert_html_dumped 'test/system/users_test.rb-29'
-        assert_html_dumped 'test/system/users_test.rb-32'
-        assert_html_dumped 'test/system/users_test.rb-36'
-        assert_html_dumped 'test/system/users_test.rb-37'
+        case ENV['TEST_FRAMEWORK']
+        when 'rspec'
+          assert_html_dumped 'spec/system/users_spec.rb-11'
+          assert_html_dumped 'spec/system/users_spec.rb-16'
+          assert_html_dumped 'spec/system/users_spec.rb-17'
+          assert_html_dumped 'spec/system/users_spec.rb-20'
+          assert_html_dumped 'spec/system/users_spec.rb-23'
+          assert_html_dumped 'spec/system/users_spec.rb-27'
+          assert_html_dumped 'spec/system/users_spec.rb-28'
+          assert_html_dumped 'spec/system/users_spec.rb-31'
+          assert_html_dumped 'spec/system/users_spec.rb-34'
+          assert_html_dumped 'spec/system/users_spec.rb-38'
+          assert_html_dumped 'spec/system/users_spec.rb-39'
+        else
+          assert_html_dumped 'test/system/users_test.rb-9'
+          assert_html_dumped 'test/system/users_test.rb-14'
+          assert_html_dumped 'test/system/users_test.rb-15'
+          assert_html_dumped 'test/system/users_test.rb-18'
+          assert_html_dumped 'test/system/users_test.rb-21'
+          assert_html_dumped 'test/system/users_test.rb-25'
+          assert_html_dumped 'test/system/users_test.rb-26'
+          assert_html_dumped 'test/system/users_test.rb-29'
+          assert_html_dumped 'test/system/users_test.rb-32'
+          assert_html_dumped 'test/system/users_test.rb-36'
+          assert_html_dumped 'test/system/users_test.rb-37'
+        end
       end
     end
   end
