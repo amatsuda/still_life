982f7f7497f3d55fa6de4afe961a7b823e6e1fa1
Akira Matsuda <ronnie@dio.jp>
2019-03-03 00:03:16 +0900

    Performance over maintainability here

--- lib/still_life/action_dispatch_extension.rb
+++ lib/still_life/action_dispatch_extension.rb
@@ -2,11 +2,33 @@
 
 module StillLife
   module ActionDispatchExtension
-    %W(get post put patch delete).each do |meth|
-      define_method meth do |*args|
-        super(*args).tap do
-          StillLife.draw(response.body)
-        end
+    def get(*args)
+      super(*args).tap do
+        StillLife.draw(response.body)
+      end
+    end
+
+    def post(*args)
+      super(*args).tap do
+        StillLife.draw(response.body)
+      end
+    end
+
+    def put(*args)
+      super(*args).tap do
+        StillLife.draw(response.body)
+      end
+    end
+
+    def patch(*args)
+      super(*args).tap do
+        StillLife.draw(response.body)
+      end
+    end
+
+    def delete(*args)
+      super(*args).tap do
+        StillLife.draw(response.body)
       end
     end
   end
