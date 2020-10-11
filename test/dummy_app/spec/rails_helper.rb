107fd364e290210400a60b1a4e9ba0a917ab0876
Akira Matsuda <ronnie@dio.jp>
2020-10-12 04:05:34 +0900

    I guess I'm doing the same thing...

--- test/dummy_app/spec/rails_helper.rb
+++ test/dummy_app/spec/rails_helper.rb
@@ -65,6 +65,11 @@ RSpec.configure do |config|
 end
 
 Capybara.register_driver :headless_chrome do |app|
-  Capybara::Selenium::Driver.new(app, browser: :chrome, args: %w[headless no-sandbox disable-dev-shm-usage disable-gpu])
+  options = ::Selenium::WebDriver::Chrome::Options.new
+  options.add_argument '--headless'
+  options.add_argument '--no-sandbox'
+  options.add_argument '--disable-dev-shm-usage'
+  Capybara::Selenium::Driver.new app, browser: :chrome, options: options
 end
+
 Capybara.default_driver = :headless_chrome
