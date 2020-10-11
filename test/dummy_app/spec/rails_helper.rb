8d135732d79d02c0d37835fd688efe677a8829bb
Akira Matsuda <ronnie@dio.jp>
2020-10-12 03:39:47 +0900

    Let's explicitly specify headless_chrome

--- test/dummy_app/spec/rails_helper.rb
+++ test/dummy_app/spec/rails_helper.rb
@@ -58,6 +58,10 @@ RSpec.configure do |config|
   config.filter_rails_from_backtrace!
   # arbitrary gems may also be filtered via:
   # config.filter_gems_from_backtrace("gem name")
+
+  config.before :each, type: :system, js: true do
+    driven_by :headless_chrome
+  end
 end
 
 Capybara.register_driver :headless_chrome do |app|
