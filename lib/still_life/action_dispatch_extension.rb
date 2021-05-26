92a9999f58a0d8b069515740c35cf12aa0fd9c28
Akira Matsuda <ronnie@dio.jp>
2021-05-27 04:39:33 +0900

    Skip processing binary files
    we can assume that files sent via `send_data` always have this header
    https://github.com/rails/rails/blob/5a8d7d90/actionpack/lib/action_controller/metal/data_streaming.rb#L140

--- lib/still_life/action_dispatch_extension.rb
+++ lib/still_life/action_dispatch_extension.rb
@@ -4,31 +4,31 @@ module StillLife
   module ActionDispatchExtension
     def get(*, **)
       super.tap do
-        StillLife.draw(response.body)
+        StillLife.draw(response.body) unless response.get_header('Content-Transfer-Encoding') == 'binary'
       end
     end
 
     def post(*, **)
       super.tap do
-        StillLife.draw(response.body)
+        StillLife.draw(response.body) unless response.get_header('Content-Transfer-Encoding') == 'binary'
       end
     end
 
     def put(*, **)
       super.tap do
-        StillLife.draw(response.body)
+        StillLife.draw(response.body) unless response.get_header('Content-Transfer-Encoding') == 'binary'
       end
     end
 
     def patch(*, **)
       super.tap do
-        StillLife.draw(response.body)
+        StillLife.draw(response.body) unless response.get_header('Content-Transfer-Encoding') == 'binary'
       end
     end
 
     def delete(*, **)
       super.tap do
-        StillLife.draw(response.body)
+        StillLife.draw(response.body) unless response.get_header('Content-Transfer-Encoding') == 'binary'
       end
     end
   end
