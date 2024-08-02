31145c93ec1726f08833872d9011dfc5108e4dc9
Akira Matsuda <ronnie@dio.jp>
2024-08-03 00:12:40 +0900

    Run newer headless chrome

--- test/dummy_app/spec/rails_helper.rb
+++ test/dummy_app/spec/rails_helper.rb
@@ -66,7 +66,7 @@ end
 
 Capybara.register_driver :headless_chrome do |app|
   options = ::Selenium::WebDriver::Chrome::Options.new
-  options.add_argument '--headless'
+  options.add_argument '--headless=new'
   options.add_argument '--no-sandbox'
   options.add_argument '--disable-dev-shm-usage'
   options.add_argument '--disable-gpu'
