71710499e6be528115475c3f3108d6f28b8e0844
Akira Matsuda <ronnie@dio.jp>
2024-08-02 23:58:16 +0900

    Log everything for all and any version of Rails

--- test/dummy_app/config/environments/test.rb
+++ test/dummy_app/config/environments/test.rb
@@ -28,6 +28,8 @@ Rails.application.configure do
   # Disable request forgery protection in test environment.
   config.action_controller.allow_forgery_protection = false
 
+  config.log_level = :debug
+
   config.action_mailer.perform_caching = false
 
   # Tell Action Mailer not to deliver emails to the real world.
